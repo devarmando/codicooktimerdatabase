548f24da69d6b77c18442632c6645ab3beb298fc
devrmando <37464589+devarmando@users.noreply.github.com>
2018-05-09 11:04:32 +0800

    Update settings.py

--- TestingPythonDatabase/settings.py
+++ TestingPythonDatabase/settings.py
@@ -127,3 +127,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+import django_heroku
+django_heroku.settings(locals())
+
